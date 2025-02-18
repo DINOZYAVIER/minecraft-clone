580e558888e1e24ce4700405c3ac9a86e7501cba
Arthur Konovalov <dinozyavier@outlook.com>
2025-02-17 19:43:56 -0700

    Add player movement and player camera

---
+++ scenes/scripts/player_character.gd
@@ -0,0 +1,39 @@
+extends CharacterBody3D
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+# Get the gravity from the project settings to be synced with RigidBody nodes.
+var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
+var sensitivity = 0.002
+
+@onready var camera_3d = $Camera3D
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _unhandled_input(event):
+	if event is InputEventMouseMotion:
+		rotation.y = rotation.y - event.relative.x * sensitivity
+		camera_3d.rotation.x = camera_3d.rotation.x - event.relative.y * sensitivity
+		camera_3d.rotation.x = clamp(camera_3d.rotation.x, deg_to_rad(-70), deg_to_rad(80))
+		
+func _physics_process(delta):
+	# Add the gravity.
+	if not is_on_floor():
+		velocity.y -= gravity * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	var input_dir = Input.get_vector("left", "right", "up", "down")
+	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction != Vector3.ZERO:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = 0
+		velocity.z = 0
+
+	move_and_slide()
