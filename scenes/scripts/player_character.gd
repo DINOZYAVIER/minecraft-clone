74cb498ef7e912c38b08833626be3467b9236d1e
Arthur Konovalov <dinozyavier@outlook.com>
2025-03-07 20:11:07 -0700

    Implement simple non-customizable hotbar

--- scenes/scripts/player_character.gd
+++ scenes/scripts/player_character.gd
@@ -6,9 +6,11 @@ const JUMP_VELOCITY = 4.5
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 var sensitivity = 0.002
+var selected = 6
 
 @onready var camera_3d = $Camera3D
 @onready var ray_cast_3d = $Camera3D/RayCast3D
+@onready var hotbar = $Hotbar
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
@@ -45,7 +47,23 @@ func _physics_process(delta):
 
 	if Input.is_action_just_pressed("right_click"):
 		if ray_cast_3d.is_colliding():
-			if ray_cast_3d.get_collider().has_method("destroy_block"):
+			if ray_cast_3d.get_collider().has_method("place_block"):
 				ray_cast_3d.get_collider().place_block(ray_cast_3d.get_collision_point() + 
-				ray_cast_3d.get_collision_normal(), 6)
+				ray_cast_3d.get_collision_normal(), selected)
+				
+	if Input.is_action_just_pressed("HotbarKey1"):
+		selected = 8
+		hotbar.select(0)
+	if Input.is_action_just_pressed("HotbarKey2"):
+		selected = 10
+		hotbar.select(1)	
+	if Input.is_action_just_pressed("HotbarKey3"):
+		selected = 15
+		hotbar.select(2)	
+	if Input.is_action_just_pressed("HotbarKey4"):
+		selected = 16
+		hotbar.select(3)	
+	if Input.is_action_just_pressed("HotbarKey5"):
+		selected = 17
+		hotbar.select(4)					
 	move_and_slide()
